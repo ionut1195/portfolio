03049cbeb0d331ee0cd3d9b2ddb9d32ef6b00729
Tomescu Ionut <ionut_tomescu95@yahoo.com>
2024-07-16 09:27:59 +0200

    placeholder landing page

---
+++ src/app/page.tsx
@@ -0,0 +1,40 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col text-center p-24">
+      <div className="container">
+        <h1>Site Under Construction</h1>
+        <p>My portfolio website is currently under development.</p>
+        <p>
+          In the meantime, you can contact me on{" "}
+          <div className="flex items-center justify-center">
+            <svg
+              className="h-5 w-5"
+              viewBox="0 0 16 16"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+            >
+              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g
+                id="SVGRepo_tracerCarrier"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></g>
+              <g id="SVGRepo_iconCarrier">
+                <path
+                  fill="#0A66C2"
+                  d="M12.225 12.225h-1.778V9.44c0-.664-.012-1.519-.925-1.519-.926 0-1.068.724-1.068 1.47v2.834H6.676V6.498h1.707v.783h.024c.348-.594.996-.95 1.684-.925 1.802 0 2.135 1.185 2.135 2.728l-.001 3.14zM4.67 5.715a1.037 1.037 0 01-1.032-1.031c0-.566.466-1.032 1.032-1.032.566 0 1.031.466 1.032 1.032 0 .566-.466 1.032-1.032 1.032zm.889 6.51h-1.78V6.498h1.78v5.727zM13.11 2H2.885A.88.88 0 002 2.866v10.268a.88.88 0 00.885.866h10.226a.882.882 0 00.889-.866V2.865a.88.88 0 00-.889-.864z"
+                ></path>
+              </g>
+            </svg>
+            <a
+              href="https://www.linkedin.com/in/ionut-tomescu-03b83bb1/"
+              target="_blank"
+            >
+              LinkedIn
+            </a>
+          </div>
+        </p>
+      </div>
+    </main>
+  );
+}
